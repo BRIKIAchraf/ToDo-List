6ff2a27530d7a9d202c376c1e1906606394e3738
AchrafbrekiDSI2022 <brikiachraf@DJERBA.r-iset.tn>
2023-09-12 23:11:43 +0200

    make the CRUD Methode

--- src/app/Component/dashboard/dashboard.component.ts
+++ src/app/Component/dashboard/dashboard.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component , OnInit } from '@angular/core';
 import { Task } from 'src/app/model/task';
 import { CrudService } from 'src/app/service/crud.service';
 @Component({
@@ -6,39 +6,65 @@ import { CrudService } from 'src/app/service/crud.service';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent {
+
+export class DashboardComponent implements OnInit {
 
   taskObj : Task = new Task();
-  taskArray : Task[] = []; // To store the data from the API
-  addTaskValue : string = ''; // To store the value from the input field
+  taskArr : Task[] = [];
 
-  constructor (private crudService : CrudService) {
-       this.crudService.getAllTask().subscribe((data)=>{
-         console.log(data);
-       }
-        );
-  }
+  addTaskValue : string = '';
+  editTaskValue : string = '';
+
+  constructor(private crudService : CrudService) { }
 
   ngOnInit(): void {
-    this.taskObj = new Task(); // To clear the input field
-    this.crudService.getAllTask();
-   }
-
-   getAllTask() {
-     this.crudService.getAllTask().subscribe((response) => {
-      this.taskArray = response;
-     },err => {
-        alert("unable to get the data");
+    this.editTaskValue = '';
+    this.addTaskValue = '';
+    this.taskObj = new Task();
+    this.taskArr = [];
+    this.getAllTask();
+  }
+  getAllTask() {
+    this.crudService.getAllTask().subscribe(res => {
+      this.taskArr = res;
+    }, err => {
+      alert("Unable to get list of tasks");
     });
-   }
+  }
 
+  addTask() {
+    this.taskObj.task_name = this.addTaskValue;
+    this.crudService.addTask(this.taskObj).subscribe(res => {
+      this.ngOnInit();
+      this.addTaskValue = '';
+    }, err => {
+      alert(err);
+    })
+  }
 
-  addTask(etask : Task) {
-   this.crudService.addTask(etask).subscribe(res =>{
-   this.ngOnInit();
-  },err => {
-  alert(err );
+  editTask() {
+    this.taskObj.task_name = this.editTaskValue;
+    this.crudService.editTask(this.taskObj).subscribe(res => {
+      this.ngOnInit();
+    }, err=> {
+      alert("Failed to update task");
+    })
+  }
+
+  deleteTask(etask : Task) {
+    this.crudService.deleteTask(etask).subscribe(res => {
+      this.ngOnInit();
+    }, err=> {
+      alert("Failed to delete task");
     });
-   }
-}
- 
+  }
+
+  call(etask : Task) {
+    this.taskObj = etask;
+    this.editTaskValue = etask.task_name;
+  }
+
+
+
+  }
+
